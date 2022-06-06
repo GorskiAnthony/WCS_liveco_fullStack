8f329d8b119dce33439086ec937d0ca4b5ff9b26
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-06-06 18:11:34 +0200

    TP Auth start is done.. Now,we will now take care of the authentication between the frontend and backend

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import img from "../assets/img/pattern-divider-desktop.svg";
 import buttonImg from "../assets/img/icon-dice.svg";
 
 const Card = () => {
+  const [advice, setAdvice] = useState({});
+  const getData = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((res) => {
+        setAdvice(res.data.slip);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleClick = () => {
+    getData();
+  };
+
   return (
     <section className="card">
       <h1 className="advice">
-        Advice <span className="id-advice">#117</span>
+        Advice <span className="id-advice"># {advice.id}</span>
       </h1>
       <div className="text-container">
-        "
-        <span className="text">
-          common regret in life is wishing one hadn't worked so hard.
-        </span>
-        "
+        "<span className="text">{advice.advice}</span>"
       </div>
       <img src={img} alt="divider" className="divider" />
-      <button>
+      <button onClick={handleClick}>
         <img src={buttonImg} alt="random advice" />
       </button>
     </section>
